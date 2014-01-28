a8dc64991b77681e794bfe1d22338c3df965d38d
Yasuyuki Oka <yasuyk@gmail.com>
2014-01-28 21:00:47 +0900

    ELPA compatibility
    - add file header
    - add provide form
    - add footer line
    - add autoload cookie to qdb function

--- qdb.el
+++ qdb.el
@@ -1,5 +1,29 @@
-;; ======================================================================
-;; qdb (Python remote debugger) functions
+;;; qdb.el --- Python remote debugger functions
+
+;; Copyright (C)  2013, 2014 Austin Bingham <austin.bingham@gmail.com>
+
+;; Author: Austin Bingham <austin.bingham@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/abingham/qdb.el
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
 
 (require 'gud)
 
@@ -78,6 +102,7 @@ This should be an executable on your path, or an absolute file name."
   :type 'string
   :group 'gud)
 
+;;;###autoload
 (defun qdb (command-line)
   "Run qdb on program FILE in buffer `*gud-FILE*'.
 The directory containing FILE becomes the initial working directory
@@ -104,3 +129,7 @@ and source-file directory for your debugger."
   (setq comint-prompt-regexp "^ *(Cmd) *")
   (setq paragraph-start comint-prompt-regexp)
   (run-hooks 'qdb-mode-hook))
+
+(provide 'qdb)
+
+;;; qdb.el ends here
